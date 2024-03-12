1127dbc335d06a48126ed363b557e9556823fe92
Burachat <s6502041510085@email.kmutnb.ac.th>
2024-03-12 13:42:58 +0700

    commit

--- src/api/student/content-types/student/lifecycles.js
+++ src/api/student/content-types/student/lifecycles.js
@@ -1,24 +1,32 @@
-const crypto = require('crypto');
+const md5 = require('md5')
+const cryptoJs = require('crypto-js')
+const cryptoKey = 'my-secret-key'
 
+const appId = 'api::student.student'
 module.exports = {
     async beforeCreate(event) {
-        console.log('beforeCreate is working...', event.params.data);
+        // console.log('beforeCreate is worked...', event.params.data)
+        // event.params.data.mobile = 'demo-hook'
+        // event.params.data.mobile = md5(event.params.data.mobile)
+        const mobile = event.params.data.mobile
+        // const makeEncrypt = cryptoJs.AES.encrypt(mobile, cryptoKey).toString()
         
-        // เพิ่มการเข้ารหัสค่า mobile โดยใช้ SHA-256 อยากได้เท่าไหร่ก็ใส่เกลือเอา แค่นั้นแหละ*0*
-        const hashedMobile = crypto.createHash('sha512').update(event.params.data.mobile).digest('hex');
-        event.params.data.mobile = hashedMobile;
-        return event; // ส่งข้อมูลที่ถูกปรับแก้ไขกลับไปยัง Sequelize
+        event.params.data.mobile = await strapi.service(appId).encrypt(mobile)
+    },
+    async afterFindOne(event) {
+        // console.log('afterFindOne ', event.result.mobile)
+
+        const mobile = event.result.mobile
+        // const makeDecrypt = cryptoJs.AES.decrypt(mobile, cryptoKey)
+        // const makeToString = makeDecrypt.toString(cryptoJs.enc.Utf8)
+        event.result.mobile = await strapi.service(appId).decrypt(mobile)
+    },
+    async afterFindMany(event) {
+        console.log('afterFindMany ', event.result)
+        event?.result?.map(async result => {
+            console.log('result ', result)
+            result.mobile = await strapi.service(appId).decrypt(result.mobile)
+            return result 
+        })
     }
-};
-// ฟังก์ชันสำหรับแสดงตัวเลขที่ถูกเข้ารหัส
-function displayHashedMobile(hashedMobile) {
-    // ทำตามความเหมือนหลังจากการเข้ารหัส (hashed) ไม่สามารถถอดรหัสกลับได้
-    // แต่เพื่อแสดงตัวอย่างเท่านั้น
-    return hashedMobile.substring(0, 6) + '*****'; // แสดงตัวอย่าง 6 ตัวแรกและเครื่องหมาย * หลังจากนั้น
 }
-
-// ตัวอย่างการเรียกใช้ฟังก์ชัน
-const hashedMobile = "f9635b4619fdf3006bf500c7b0a21f5c1b91f80b3bf525afe96614d666778956d8fc666d7a0d421d6b5f355ec76a428a9fa131b5ab57d1bbf8631cc770c587b2";
-const displayedMobile = displayHashedMobile(hashedMobile);
-console.log(displayedMobile);
-
--- src/api/student/services/student.js
+++ src/api/student/services/student.js
@@ -3,7 +3,16 @@
 /**
  * student service
  */
-
 const { createCoreService } = require('@strapi/strapi').factories;
+const cryptoJs = require('crypto-js')
+const cryptoKey = 'my-secret-key'
 
-module.exports = createCoreService('api::student.student');
+module.exports = createCoreService('api::student.student', ({ strapi }) => ({
+    async encrypt(data) {
+        return cryptoJs.AES.encrypt(data, cryptoKey).toString()
+    },
+    async decrypt(data) {
+        const makeDecrypt = cryptoJs.AES.decrypt(data, cryptoKey)
+        return makeDecrypt.toString(cryptoJs.enc.Utf8)
+    }
+}));
